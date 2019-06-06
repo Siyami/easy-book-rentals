0ceec04eb14934f13db349622bc4e64a14c55b15
Siyami Avci <siyamiavci@gmail.com>
2019-06-05 23:16:31 -0700

    add router module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { RouterModule, Routes } from '@angular/router';
 
 import { AppComponent } from './app.component';
 import { ShoppingCartComponent } from './components/shopping-cart/shopping-cart.component';
@@ -12,7 +12,12 @@ import { ShoppingCartComponent } from './components/shopping-cart/shopping-cart.
     ShoppingCartComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule.forRoot([
+      { path: 'shopping-cart', component: ShoppingCartComponent },
+      // { path: '', redirectTo: 'rules', pathMatch: 'full' },
+      // { path: '**', redirectTo: 'rules', pathMatch: 'full' }
+    ])
   ],
   providers: [],
   bootstrap: [AppComponent]
